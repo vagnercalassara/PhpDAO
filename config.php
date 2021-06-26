17d620fa78fde219fd410b980b0c50d43ef99405
Vagner <vagnercalassara@gmail.com>
2021-06-26 16:02:39 -0300

    Criação da classe config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 spl_autoload_register(
 	function($class_name)
 	{
-	 	$filename = $class_name.".php";
+	 	$filename = "Class".DIRECTORY_SEPARATOR.$class_name.".php";
 		
 		if (file_exists($filename)) {
 			require_once($filename);
